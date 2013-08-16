82f71a3def84b6f2b17f6740ac17442ac1c4d69e
Matthew Barry <komputerwiz.matt@gmail.com>
2013-08-16 15:11:37 -0500

    Files from TCAT config

---
+++ config/autostart/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# autostart script run by autostart.desktop
+
+# mouse speed is too fast by default
+# xinput set-prop "PixArt USB Optical Mouse" "Device Accel Constant Deceleration" 3 &
