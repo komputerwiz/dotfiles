1cb05c31d75c88868cacb924617d39b2b38b3a56
Matthew Barry <komputerwiz.matt@gmail.com>
2015-07-02 12:04:35 -0500

    END OF ERA; USE GNU STOW FROM NOW ON

--- config/autostart/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# autostart script run by autostart.desktop
-
-# mouse speed is too fast by default
-# xinput set-prop "PixArt USB Optical Mouse" "Device Accel Constant Deceleration" 3 &
--- config/dmenu/dmenu-bind.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-exe=$(dmenu_run -nb '#000000' -nf '#DDDDDD' -sb '#3366FF' -sf '#FFFFFF') && eval "exec $exe"
