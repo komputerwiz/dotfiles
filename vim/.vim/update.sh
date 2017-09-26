22e5121ccdd9b3af83a57d7eadce7a8efcadeaeb
Matthew Barry <mbarry@tamu.edu>
2017-09-26 15:37:17 -0500

    [vim] simplify update script

--- vim/.vim/update.sh
+++ vim/.vim/update.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-DIR="${0%/*}"
-DIR="`cd "${DIR}" && pwd`"
-
-cd "${DIR}"
-
-vim +PlugClean +PluginInstall +PlugUpdate +qall
-
-unset DIR
+vim +PlugClean +PlugUpdate +PlugInstall +qall
