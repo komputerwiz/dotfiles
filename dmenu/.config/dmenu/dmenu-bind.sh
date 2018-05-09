e552fbada2bb11825e0be917676f06a87626d39e
Matthew Barry <mbarry@tamu.edu>
2018-05-09 18:31:57 -0500

    [dmenu] update configuration

--- dmenu/.config/dmenu/dmenu-bind.sh
+++ dmenu/.config/dmenu/dmenu-bind.sh
@@ -3,4 +3,5 @@ bgcolor='#002B36' #000000
 fgcolor='#839496' #DDDDDD
 bgselectcolor='#073642' #3366FF
 fgselectcolor='#268BD2' #FFFFFF
-exe=$(dmenu_run -nb "$bgcolor" -nf "$fgcolor" -sb "$bgselectcolor" -sf "$fgselectcolor") && eval "exec $exe"
+font='Ubuntu Mono-9'
+exe=$(dmenu_run -fn "$font" -nb "$bgcolor" -nf "$fgcolor" -sb "$bgselectcolor" -sf "$fgselectcolor") && eval "exec $exe"
