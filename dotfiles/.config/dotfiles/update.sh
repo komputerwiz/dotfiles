e7b4fd8cec388a47f09c813d65cef600b840c06c
Matthew Barry <komputerwiz.matt@gmail.com>
2019-02-20 21:53:47 -0600

    use lockfile to prevent simultaneous update invocations

--- dotfiles/.config/dotfiles/update.sh
+++ dotfiles/.config/dotfiles/update.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -e
 
 FREQ=3 # days
+DATA_DIR="${XDG_DATA_HOME:-"$HOME/.local/share"}/dotfiles"
+[ -d "$DATA_DIR" ] || mkdir -p "$DATA_DIR"
+
+# prevent multiple instances from runinng simultaneously
+LOCKFILE="$DATA_DIR/update.lock"
+[ -f "$LOCKFILE" ] && echo "update script already running" >&2 && exit 1
+touch "$LOCKFILE"
+unlock () { rm -f "$LOCKFILE"; }
+trap unlock EXIT
 
 # attempt to load last update date from file
-EPOCH_FILE="${XDG_DATA_HOME:-"$HOME/.local/share"}/dotfiles/update_epoch"
+EPOCH_FILE="$DATA_DIR/update_epoch"
 [ -f "$EPOCH_FILE" ] && LAST_RUN="$(cat "$EPOCH_FILE")"
 
 NOW=$(( $(date +%s) / 60 / 60 / 24 ))
@@ -20,7 +30,6 @@ for SCRIPT in "$DIR"/update.d/*; do
 done
 
 # save epoch to file
-[ ! -d "$(dirname "$EPOCH_FILE")" ] && mkdir -p "$(dirname "$EPOCH_FILE")"
 echo "$NOW" > "$EPOCH_FILE"
 
 sleep 3
