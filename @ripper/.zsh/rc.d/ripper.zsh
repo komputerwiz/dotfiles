b0df5295ab219f5f7161cda88d43c47c57912794
Matthew Barry <komputerwiz.matt@gmail.com>
2020-07-20 09:39:16 -0500

    [@ripper] replace aliases with executable scripts

--- @ripper/.zsh/rc.d/ripper.zsh
+++
@@ -1,6 +0,0 @@
-alias startvpn='sudo systemctl start tamuvpn.service'
-alias fvpn='sudo systemctl restart tamuvpn.service'
-alias stopvpn='sudo systemctl stop tamuvpn.service'
-
-alias vaultopen='sudo systemctl start mnt-vault.mount'
-alias vaultclose='sudo systemctl stop systemd-cryptsetup@vault.service'
