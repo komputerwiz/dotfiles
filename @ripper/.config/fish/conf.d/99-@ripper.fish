3c1974c4f60d0771b59c6a252461ce091d4370c6
Matthew Barry <komputerwiz.matt@gmail.com>
2021-01-14 12:51:39 -0600

    [@ripper] add alias to set TERM=xterm for 'alot' program

--- @ripper/.config/fish/conf.d/99-@ripper.fish
+++ @ripper/.config/fish/conf.d/99-@ripper.fish
@@ -1,3 +1,8 @@
 if test -n "$DESKTOP_SESSION"
   set -x (gnome-keyring-daemon --start | string split '=')
 end
+
+function alot --wraps 'alot' --description 'run alot with TERM=xterm to fix terminal font color rendering'
+  set -lx TERM xterm
+  command alot $argv
+end
