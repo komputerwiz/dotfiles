3a7d96eaa9d952ad532929a0ad5b887ee154ff7e
Matthew Barry <mbarry@tamu.edu>
2021-12-16 08:47:28 -0600

    [dotfiles] always force updates if run explicitly; use '-a' flag for 'auto' mode to test frequency

--- dotfiles/.local/bin/update.sh
+++ dotfiles/.local/bin/update.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -e
 
-FORCE=false
 DRY=false
 
 print_help () {
   echo "Usage: $0 [OPTIONS]"
   echo ""
   echo "  Options:"
-  echo "    -f   Force update even if it was run recently."
-  echo "    -n   Dry-run; don't actually execute scripts; just show what would be run."
-  echo "    -h   Display this help message."
+  echo "    -a DAYS  Automatic mode: only update if not run in the given number of DAYS."
+  echo "    -n       Dry-run; don't actually execute scripts; just show what would be run."
+  echo "    -h       Display this help message."
 }
 
-while getopts ":hfn" opt; do
+while getopts ":a:hn" opt; do
   case "$opt" in
-    f) FORCE=true ;;
+    a)
+      FREQ="$OPTARG"
+      RE_NUMBER='^[0-9]+$'
+      if ! [[ "$FREQ" =~ $RE_NUMBER ]]; then
+        echo "Argument -a expected number, received '$FREQ'" >&2
+        exit 1
+      fi
+      ;;
     n) DRY=true ;;
     h)
       print_help
@@ -28,7 +34,6 @@ while getopts ":hfn" opt; do
   esac
 done
 
-FREQ=3 # days
 DATA_DIR="${XDG_DATA_HOME:-"$HOME/.local/share"}/dotfiles"
 if test ! -d "$DATA_DIR"; then
   mkdir -p "$DATA_DIR"
@@ -52,8 +57,8 @@ if test -f "$EPOCH_FILE"; then
 fi
 
 NOW=$(( $(date +%s) / 60 / 60 / 24 ))
-if test -n "$LAST_RUN"; then
-  if test "$(expr "$NOW" - "$LAST_RUN" )" -lt "$FREQ" -a "$FORCE" != true; then
+if test -n "$LAST_RUN" -a -n "$FREQ"; then
+  if test "$(expr "$NOW" - "$LAST_RUN" )" -lt "$FREQ"; then
     echo "update was run recently; exiting"
     exit 0
   fi
@@ -73,5 +78,3 @@ done
 
 # save epoch to file
 echo "$NOW" > "$EPOCH_FILE"
-
-sleep 3
