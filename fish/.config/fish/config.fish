8f5c01118dcd1d4214b06290c5561110277c52f0
Matthew Barry <komputerwiz.matt@gmail.com>
2022-03-28 22:40:49 -0500

    [fish] move 'dr' alias to function file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,7 +12,3 @@ alias mv='mv -i'
 
 alias h='history'
 alias hs='history | grep'
-
-function dr --wraps docker --description 'run ephemeral docker container in current directory'
-  docker run --interactive --tty --rm --volume "$PWD:/data:rw" --workdir /data $argv
-end
