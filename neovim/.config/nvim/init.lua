f947ea21b63dbe0d7d8fd46f837ba63ef1b3805a
Matthew Barry <komputerwiz.matt@gmail.com>
2022-04-27 00:31:29 -0500

    [neovim] update recommended config for nvim-cmp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -239,9 +239,11 @@ cmp.setup({
     end,
   },
 
-  mapping = {
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- set to 'false' to force selection
     ['<C-e>'] = cmp.mapping({
       i = function (fallback)
         if luasnip.choice_active() then
@@ -255,8 +257,7 @@ cmp.setup({
 
     ['<Tab>'] = cmp.mapping(function (fallback)
       if cmp.visible() then
-        -- cmp.select_next_item()
-        cmp.confirm({ select = true })
+        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
@@ -267,17 +268,19 @@ cmp.setup({
     end, {'i', 's'}),
 
     ['<S-Tab>'] = cmp.mapping(function (fallback)
-      if luasnip.jumpable(-1) then
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
       end
     end, {'i', 's'}),
-  },
+  }),
 
   sources = cmp.config.sources({
-    {name = 'luasnip'},
     {name = 'nvim_lsp'},
+    {name = 'luasnip'},
   }, {
     {name = 'path'},
   }, {
@@ -287,12 +290,14 @@ cmp.setup({
 })
 
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     {name = 'buffer'},
   },
 })
 
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     {name = 'path'},
   }, {
@@ -476,8 +481,15 @@ if not vim.env.WORKSPACE then
   vim.env.WORKSPACE = vim.env.HOME .. '/ws/jdtls'
 end
 
+-- decorate LSP capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
 lsp_installer.on_server_ready(function (server)
-  local opts = {on_attach = on_attach}
+  local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 
   if server_opts[server.name] then
     extend(opts, server_opts[server.name])
