5e47046252280e05678af6cc311a7622fb8a9d97
Matthew Barry <komputerwiz.matt@gmail.com>
2022-09-01 21:20:29 -0500

    [fish][ytdl] fix typo

--- ytdl-fish/.config/fish/functions/ytdl.fish
+++ ytdl-fish/.config/fish/functions/ytdl.fish
@@ -4,6 +4,7 @@ function ytdl -d "wrapper for yt-dlp with fallback to youtube-dl"
   else if type -q youtube-dl
     echo "Note: consider upgrading to yt-dlp"
     youtube-dl $argv
+  else
+    echo "Error: unable to find ytdl executable"
   end
-  echo "Error: unable to find ytdl executable"
 end
