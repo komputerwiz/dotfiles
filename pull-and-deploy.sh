9287576aea8d0426691599741b9178454f93a9c0
Matthew Barry <komputerwiz.matt@gmail.com>
2022-10-25 23:04:46 -0500

    [dotter] change to install dir in update script to let dotter find files

--- pull-and-deploy.sh
+++ pull-and-deploy.sh
@@ -4,9 +4,10 @@ set -e
 INSTALL_DIR="$HOME/.dotfiles"
 DOTTER="$INSTALL_DIR/dotter"
 
-BRANCH="$(git -C "$INSTALL_DIR" rev-parse --abbrev-ref HEAD)"
+cd "$INSTALL_DIR"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
-git -C "$INSTALL_DIR" fetch
-git -C "$INSTALL_DIR" -c "user.name=Matthew Barry" -c "user.email=matthew@komputerwiz.net" rebase "origin/$BRANCH"
+git fetch
+git -c "user.name=Matthew Barry" -c "user.email=matthew@komputerwiz.net" rebase "origin/$BRANCH"
 
 "$DOTTER" deploy
