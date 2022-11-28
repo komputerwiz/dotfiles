1c8b73f2c42e20fa0928938b98ac9be2b10d7b98
Matthew Barry <komputerwiz.matt@gmail.com>
2022-11-28 15:05:07 -0600

    [dotter] create stub shell executable to detect OS/Arch for running dotter

--- pull-and-deploy.sh
+++ pull-and-deploy.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ "$OSTYPE" == darwin* ]]; then
-  OS=darwin
-elif [[ "$OSTYPE" = linux-gnu* ]]; then
-  OS=linux
-fi
-
-ARCH="$HOSTTYPE"
-
 INSTALL_DIR="$HOME/.dotfiles"
-DOTTER="$INSTALL_DIR/.dotter/bin/dotter-$OS-$ARCH"
-
-if test ! -x "$DOTTER"; then
-  echo "dotter exec not found for OS ($OS) and arch ($ARCH)" >&2
-  exit
-fi
 
 cd "$INSTALL_DIR"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
@@ -23,4 +9,4 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 git fetch
 git -c "user.name=Matthew Barry" -c "user.email=matthew@komputerwiz.net" rebase "origin/$BRANCH"
 
-"$DOTTER" deploy
+"$INSTALL_DIR/.dotter/bin/dotter" deploy
