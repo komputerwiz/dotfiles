fcd0fa2e77b020cf07e004cd17d7465410180d2b
Matthew Barry <komputerwiz.matt@gmail.com>
2022-12-03 01:35:21 -0600

    [nvim] add paq bootstrap

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -1,5 +1,16 @@
 -- {{{ plugins
 
+-- {{{ paq bootstrap
+
+do
+	local path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+	if vim.fn.empty(vim.fn.glob(path)) > 0 then
+		vim.fn.system({ 'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', path })
+	end
+end
+
+-- }}}
+
 require('paq')({
 	-- {{{ lua plugins
 
