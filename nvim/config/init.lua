f216ba964f590f5c84edc9b86deb988f12b57c37
Matthew Barry <komputerwiz.matt@gmail.com>
2024-02-01 23:03:15 -0600

    [nvim] inline config, fix NeoSolarized config, update spell list

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -89,19 +89,8 @@ require('paq')({
 })
 
 -- }}}
--- {{{ syntax highlighting
-
-vim.cmd('colorscheme NeoSolarized')
-
--- force whitespace highlighting to look like comments
-vim.cmd([[
-	augroup mysyntax
-		autocmd! Syntax,BufNewFile,BufReadPost * highlight link Whitespace Comment
-	augroup END
-]])
-
--- }}}
 -- neovide gui {{{
+
 if vim.g.neovide then
 	vim.o.guifont = 'FiraCode Nerd Font:h9'
 	vim.g.neovide_scale_factor = 1.0
@@ -109,6 +98,7 @@ if vim.g.neovide then
 	vim.g.neovide_floating_blur_amount_x = 3.0
 	vim.g.neovide_floating_blur_amount_y = 3.0
 end
+
 -- }}}
 -- {{{ options
 
@@ -161,19 +151,17 @@ local dap = require('dap')
 local dap_ext_vscode = require('dap.ext.vscode')
 local dap_ui = require('dapui')
 local gitsigns = require('gitsigns')
+local lspconfig = require('lspconfig')
 local lualine = require('lualine')
 local luasnip = require('luasnip')
 local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
 local mason_registry = require('mason-registry')
 local oil = require('oil')
 local surround = require('nvim-surround')
 local telescope = require('telescope')
 local telescope_builtin = require('telescope.builtin')
 local tsconfigs = require('nvim-treesitter.configs')
-
--- local modules
-local language_servers = require('language-servers')
-local snippets = require('snippets')
 
 -- }}}
 -- {{{ key mappings
@@ -328,9 +316,481 @@ lualine.setup({
 -- }}}
 -- {{{ luasnip
 
--- this configuration is too large and complex to fit here and has been broken
--- into a separate file
-snippets.setup()
+do
+	local ls = luasnip
+	local types = require('luasnip.util.types')
+
+	-- {{{ shorthand variables
+
+	local s = ls.snippet
+	local sn = ls.snippet_node
+	local t = ls.text_node
+	local i = ls.insert_node
+	local f = ls.function_node
+	local c = ls.choice_node
+	local d = ls.dynamic_node
+	local r = ls.restore_node
+
+	-- }}}
+	-- {{{ helper functions
+
+	-- copies the text value of another text field
+	local function copy(args)
+		return args[1]
+	end
+
+	-- traditional functional map operation
+	local function map(tbl, fn)
+		local rv = {}
+		for key, val in pairs(tbl) do
+			rv[key] = fn(val)
+		end
+		return rv
+	end
+
+	-- capitalize the first letter of a word
+	local function capitalize(args)
+		local capitalized = table.concat(args[1]):gsub('^%l', string.upper)
+		return sn(nil, i(1, capitalized))
+	end
+
+	-- generates a new random v4 UUID
+	local function uuid4()
+		local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+		return template:gsub('[xy]', function(ch)
+			local v = (ch == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
+			return string.format('%x', v)
+		end)
+	end
+
+	local function filename_node(num, placeholder)
+		return d(num, function()
+			local classname = vim.fn.expand('%:t:r')
+			if classname == '' then
+				-- fallback for placeholder text
+				classname = placeholder or 'MyType'
+			end
+			return sn(nil, i(1, classname))
+		end)
+	end
+
+	-- recursive delimited expansion
+	local function rec_delim(args, parent, old_state, delim, placeholder)
+		return sn(
+			nil,
+			c(1, {
+				t(''), -- putting sn(...) first causes infinite loop
+				sn(nil, {
+					t(delim),
+					i(1, placeholder),
+					d(2, rec_delim, {}, { user_args = { delim, placeholder } }),
+				}),
+			})
+		)
+	end
+
+	-- }}}
+
+	-- {{{ luasnip configuration
+
+	ls.config.set_config({
+		history = true,
+		updateevents = 'TextChanged,TextChangedI',
+		-- enable_autosnippets = true,
+		ext_opts = {
+			-- display `snip:choice` at end of line when choice node is active
+			[types.choiceNode] = {
+				active = {
+					virt_text = { { 'snip:choice (<C-e>)', 'Comment' } },
+				},
+			},
+		},
+	})
+
+	-- }}}
+	-- {{{ snippet definitions
+
+	-- {{{ global snippets
+
+	ls.add_snippets('all', {
+		s(
+			{ trig = 'date', name = 'ISO 8601 Date' },
+			f(function()
+				return os.date('%F')
+			end)
+		),
+
+		s(
+			{ trig = 'time', name = 'ISO 8601 Time' },
+			f(function()
+				return os.date('%T')
+			end)
+		),
+
+		s(
+			{ trig = 'timetz', name = 'ISO 8601 Time with Timezone Offset' },
+			f(function()
+				return os.date('%T%z')
+			end)
+		),
+
+		s(
+			{ trig = 'datetime', name = 'ISO 8601 Date and Time' },
+			f(function()
+				return os.date('%F %T')
+			end)
+		),
+
+		s(
+			{ trig = 'datetimetz', name = 'ISO 8601 Date and Time with Timezone Offset' },
+			f(function()
+				return os.date('%F %T%z')
+			end)
+		),
+
+		s('shrug', t('¯\\_(ツ)_/¯')),
+		s({ trig = 'uuid', name = 'Random UUIDv4' }, f(uuid4)),
+	})
+
+	-- }}}
+	-- {{{ c
+
+	ls.add_snippets('c', {
+		s({ trig = 'ifnd', name = '#ifndef Header Guard' }, {
+			t('#ifndef '),
+			i(1),
+			t({ '', '#define ' }),
+			f(copy, 1),
+			t({ '', '', '' }),
+			i(0),
+			t({ '', '', '' }),
+			t('#endif // '),
+			f(copy, 1),
+		}),
+	})
+
+	-- }}}
+	-- {{{ cpp
+
+	ls.add_snippets('cpp', {
+		s({
+			trig = 'ro5',
+			wordTrig = false,
+			name = 'Rule of 5',
+			dscr = 'Member functions on a class or struct whose definitions '
+					.. 'should be defined together: copy/move constructors, copy/move '
+					.. 'assignment operators, and destructor',
+		}, {
+			i(1),
+			t('('),
+			f(copy, 1),
+			t({ ' const&) = default;', '' }),
+			f(copy, 1),
+			t('('),
+			f(copy, 1),
+			t({ '&&) noexcept = default;', '' }),
+			f(copy, 1),
+			t('& operator=('),
+			f(copy, 1),
+			t({ ' const&) = default;', '' }),
+			f(copy, 1),
+			t('& operator=('),
+			f(copy, 1),
+			t({ '&&) noexcept = default;', '' }),
+			t('~'),
+			f(copy, 1),
+			t({ '() = default;', '' }),
+		}),
+	})
+
+	-- }}}
+	-- {{{ java
+
+	ls.add_snippets('java', {
+		s({ trig = 'logger', name = 'System Logger for class' }, {
+			t('private static final Logger logger = System.getLogger('),
+			filename_node(1),
+			t('.class.getName());'),
+		}),
+
+		s({ trig = 'fori', name = 'for (i)' }, {
+			t('for (int '),
+			i(1, 'i'),
+			t(' = 0; '),
+			f(copy, 1),
+			t(' < '),
+			i(2, 'max'),
+			t('; '),
+			f(copy, 1),
+			t('++)'),
+		}),
+
+		s({ trig = 'fore', name = 'for (each)' }, {
+			t('for (var '),
+			i(1, 'item'),
+			t(' : '),
+			i(2, 'collection'),
+			t(')'),
+		}),
+
+		s('class', {
+			c(1, {
+				t('public '),
+				t('private '),
+			}),
+			c(2, {
+				t(''),
+				t('static '),
+			}),
+			t('class '),
+			filename_node(3, 'MyClass'),
+			-- f(function (args) return table.concat(args[1]):gsub('..*', ' ') end, 1),
+			c(4, {
+				t(''),
+				sn(nil, {
+					t(' extends '),
+					i(1, 'BaseClass'),
+				}),
+			}),
+			c(5, {
+				t(''),
+				sn(nil, {
+					t(' implements '),
+					i(1, 'MyInterface'),
+					d(2, rec_delim, {}, {
+						user_args = { ', ', 'MyInterface' },
+					}),
+				}),
+			}),
+			t({ '', '{', '\t' }),
+			i(0),
+			t({ '', '}' }),
+		}),
+
+		s('interface', {
+			c(1, {
+				t('public '),
+				t('private '),
+			}),
+			c(2, {
+				t(''),
+				t('static '),
+			}),
+			t('interface '),
+			filename_node(3, 'MyInterface'),
+			c(4, {
+				t(''),
+				sn(nil, {
+					t(' extends '),
+					i(1, 'MyInterface'),
+					d(2, rec_delim, {}, {
+						user_args = { ', ', 'MyInterface' },
+					}),
+				}),
+			}),
+			t({ '', '{', '\t' }),
+			i(0),
+			t({ '', '}' }),
+		}),
+
+		s('main', {
+			t({ 'public static void main(String[] args)', '\t' }),
+			i(0),
+			t({ '', '}' }),
+		}),
+
+		s('method', {
+			c(1, {
+				t('public'),
+				t('protected'),
+				t('private'),
+			}),
+			c(2, {
+				t(''),
+				t(' static'),
+			}),
+			t(' '),
+			i(3, 'void'),
+			t(' '),
+			i(4, 'methodName'),
+			t('('),
+			i(5),
+			t({ ')', '{', '\t' }),
+			i(0),
+			t({ '', '}' }),
+		}),
+
+		s('getter', {
+			t('public '),
+			i(2, 'String'),
+			t(' get'),
+			d(3, capitalize, { 1 }),
+			t({ '()', '{', '\treturn ' }),
+			i(1),
+			t({ ';', '}' }),
+		}),
+
+		s('setter', {
+			t({ 'public void set' }),
+			d(3, capitalize, { 1 }),
+			t('('),
+			i(2, 'String'),
+			t(' '),
+			f(copy, 1),
+			t({ ')', '{', '\tthis.' }),
+			i(1),
+			t(' = '),
+			f(copy, 1),
+			t({ ';', '}' }),
+		}),
+	})
+
+	-- }}}
+	-- {{{ markdown
+
+	ls.add_snippets('markdown', {
+		s('qso', { t('{{% qso '), i(1), t(' %}}') }),
+		s('motion', {
+			t('{{< motion "'), i(1, "to do ..."), t('" '), i(2, 'mover'), t(' '),
+			c(3, {
+				t('/>}}'),
+				sn(nil, {
+					i(1, 'seconder'), t(' "'), i(3, 'outcome'),
+					t('" '),
+					c(2, {
+						t('/>}}'),
+						sn(nil, {
+							t({'>}}', '\t'}), i(1, 'discussion'), t({'', '{{</ motion >}}'}),
+						}),
+					}),
+				}),
+				sn(nil, {
+					t({'>}}', '\t'}), i(1, 'discussion'), t({'', '{{</ motion >}}'}),
+				}),
+			}),
+		}),
+		s('sup', { t('<sup>'), i(1), t('</sup>') }),
+		s('sub', { t('<sub>'), i(1), t('</sub>') }),
+		s({
+			trig = '#(%d+)',
+			name = 'TCAT Gitea Issue Link',
+			regTrig = true,
+			docTrig = 'Gitea Link',
+		}, {
+			t('[#'),
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			t('](https://gitea.citd.tamu.edu/TCAT/'),
+			i(1, 'project'),
+			t('/issues/'),
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			t(')'),
+		}),
+	})
+
+	-- }}}
+	-- {{{ php
+
+	ls.add_snippets('php', {
+		s('php', t({ '<?php declare(strict_types=1);', '', '' })),
+
+		s('getter', {
+			t('public function get'),
+			d(3, capitalize, { 1 }),
+			t('(): '),
+			i(2, '?string'),
+			t({ '', '{', '\treturn $this->' }),
+			i(1),
+			t({ ';', '}' }),
+		}),
+
+		s('setter', {
+			t('public function set'),
+			d(3, capitalize, { 1 }),
+			t('('),
+			i(2, '?string'),
+			t(' $'),
+			f(copy, 1),
+			t({ '): self', '{', '\t$this->' }),
+			i(1),
+			t(' = $'),
+			f(copy, 1),
+			t({ ';', '\treturn $this;', '}' }),
+		}),
+	})
+
+	-- }}}
+	-- {{{ sh
+
+	ls.add_snippets('sh', {
+		s('DIR', t({ 'DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"', '' })),
+	})
+
+	-- }}}
+	-- {{{ tex
+
+	ls.add_snippets('tex', {
+		s({ trig = 'e', name = 'TeX Environment' }, {
+			t('\\begin{'),
+			i(1),
+			t('}'),
+			t({ '', '\t' }),
+			i(0),
+			t({ '', '\\end{' }),
+			f(copy, 1),
+			t('}'),
+		}),
+
+		s({ trig = 'item', name = 'Itemized List Item' }, {
+			t('\\item '),
+			i(1),
+			d(2, rec_delim, {}, {
+				user_args = { { '', '\\item ' } },
+			}),
+		}),
+
+		s({ trig = 'f', name = 'Math Fraction' }, { t('\\frac{'), i(1), t('}{'), i(2), t('}') }),
+		s({ trig = 'm', name = 'Inline Math' }, { t('\\( '), i(1), t(' \\)') }),
+		s({ trig = 'M', name = 'Display Math' }, { t('\\[ '), i(1), t(' \\]') }),
+		s({ trig = 't', name = 'Math Text' }, { t('\\text{'), i(1), t('}') }),
+		s({ trig = '(', name = 'Matching Parentheses: ()' }, { t('\\left( '), i(1), t(' \\right)') }),
+		s({ trig = '[', name = 'Matching Square Brackets: []' }, { t('\\left[ '), i(1), t(' \\right]') }),
+		s({ trig = '{', name = 'Matching Curly Braces: {}' }, { t('\\left\\{ '), i(1), t(' \\right\\}') }),
+	})
+
+	-- }}}
+	-- {{{ xml
+
+	ls.add_snippets('xml', {
+		s({ trig = 'trans', name = 'XLIFF Translation Table Entry' }, {
+			t('<trans-unit id="'),
+			f(function(args)
+				return table.concat(args[1]):gsub('%.', '_')
+			end, { 1 }),
+			t({ '">', '\t<source>' }),
+			i(1),
+			t({ '</source>', '\t<target>' }),
+			i(2),
+			t({ '</target>', '</trans-unit>' }),
+		}),
+	})
+
+	-- }}}
+
+	-- }}}
+	-- {{{ filetype extensions
+
+	-- ls.filetype_extend('X', {'Y', 'Z'})
+	-- load snippets for 'Y' and 'Z' filetypes when editing 'X' filetype
+
+	ls.filetype_extend('cpp', { 'c' })
+	ls.filetype_extend('markdown', { 'tex' })
+
+	-- }}}
+end
 
 -- }}}
 -- {{{ markdown (built-in)
@@ -698,9 +1158,209 @@ vim.g.tex_flavor = 'latex'
 -- }}}
 -- {{{ language server (LSP) config
 
-language_servers.setup({
-	capabilities = cmp_nvim_lsp.default_capabilities(),
-})
+do
+	local capabilities = cmp_nvim_lsp.default_capabilities()
+
+	-- {{{ on_attach() - adjust settings after the language server attaches to the current buffer
+
+	local function on_attach(client, bufnr)
+		local function bmap(...)
+			vim.api.nvim_buf_set_keymap(bufnr, ...)
+		end
+
+		local function bopt(...)
+			vim.api.nvim_buf_set_option(bufnr, ...)
+		end
+
+		-- enable omni-completion (<C-x><C-o>)
+		bopt('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+		-- {{{ key mappings
+
+		-- (see `:h vim.lsp.*` for docs)
+		local opts = { noremap = true, silent = true }
+
+		-- {{{ code navigation
+
+		bmap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+		bmap('n', 'g]', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+		bmap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+		bmap('n', 'gy', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+		bmap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+
+		-- }}}
+		-- {{{ inline help
+
+		bmap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+		bmap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+
+		-- }}}
+		-- {{{ workspace management
+
+		bmap('n', '<Leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+		bmap('n', '<Leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+		bmap('n', '<Leader>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+
+		-- }}}
+		-- {{{ code manipulation
+
+		bmap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+		bmap('n', '<Leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+		bmap('n', '<Leader>=', '<Cmd>lua vim.lsp.buf.format()<CR>', opts)
+
+		-- }}}
+		-- {{{ diagnostics
+
+		bmap('n', '<Leader>d', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts)
+		bmap('n', '[d', '<Cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+		bmap('n', ']d', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+		bmap('n', '<Leader>q', '<Cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
+
+		-- }}}
+
+		-- }}}
+		-- {{{ autocmd and syntax for hovers
+
+		if client.server_capabilities.documentHighlightProvider then
+			vim.cmd([[
+				augroup lsp_document_highlight
+					autocmd!
+					autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+					autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+					autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+					autocmd Syntax * highlight link LspReferenceText CursorLine
+					autocmd Syntax * highlight link LspReferenceRead LspReferenceText
+					autocmd Syntax * highlight link LspReferenceWrite LspReferenceText
+				augroup END
+			]])
+		end
+
+		-- }}}
+	end
+
+	-- }}}
+
+	mason_lspconfig.setup_handlers({
+		-- {{{ default settings
+		function(server_name)
+			lspconfig[server_name].setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+		end,
+		-- }}}
+		clangd = function() -- {{{
+			lspconfig.clangd.setup({
+				on_attach = on_attach,
+				capabilities = vim.tbl_deep_extend('force', capabilities, {
+					offsetEncoding = {'utf-16'},
+				}),
+			})
+		end,
+		-- }}}
+		emmet_ls = function() -- {{{
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					'*html*',
+					'typescriptreact',
+					'javascriptreact',
+					'css',
+					'sass',
+					'scss',
+					'less',
+				},
+				-- init_options = {
+				-- 	html = {
+				-- 		options = {
+				-- 			["bem.enabled"] = true,
+				-- 		},
+				-- 	},
+				-- },
+			})
+		end,
+		-- }}}
+		grammarly = function () -- {{{
+			lspconfig.grammarly.setup({
+				filetypes = {
+					'*markdown*',
+					'tex',
+				},
+				init_options = {
+					grammarly = {
+						config = {
+							suggestions = {
+								ConjunctionAtStartOfSentence = true,
+								InformalPronounsAcademic = true,
+								OxfordComma = true,
+								PassiveVoice = true,
+								PrepositionAtTheEndOfSentence = true,
+								StylisticFragments = true,
+								UnnecessaryEllipses = true,
+							},
+						},
+					},
+				},
+			})
+		end,
+		-- }}}
+		intelephense = function() -- {{{
+			lspconfig.intelephense.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				init_options = {
+					licenseKey = 'XXXXXXXXXXXXXXX',
+					globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense',
+				},
+			})
+		end,
+		-- }}}
+		jdtls = function() -- {{{
+			-- workspace path
+			if not vim.env.WORKSPACE then
+				vim.env.WORKSPACE = vim.env.HOME .. '/ws/jdtls'
+			end
+
+			lspconfig.jdtls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { 'jdtls' },
+			})
+		end,
+		-- }}}
+		lua_ls = function() -- {{{
+			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = 'LuaJIT',
+						},
+						diagnostics = {
+							globals = {
+								'vim',
+							},
+						},
+					},
+				},
+			})
+		end,
+		-- }}}
+	})
+end
+
+-- }}}
+-- {{{ syntax highlighting
+
+vim.cmd('colorscheme NeoSolarized')
+
+-- force whitespace highlighting to look like comments
+vim.cmd([[
+	augroup mysyntax
+		autocmd! Syntax,BufNewFile,BufReadPost * highlight link Whitespace Comment
+	augroup END
+]])
 
 -- }}}
 -- {{{ automatically source file after editing
