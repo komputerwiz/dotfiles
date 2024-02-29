531b1858952e3f679c31bf44d73dd03796b68809
Matthew Barry <komputerwiz.matt@gmail.com>
2024-02-29 10:08:57 -0600

    [@ripper] using ssh-agent instead of gcr-ssh-agent for now

--- _host/ripper/config.fish
+++ _host/ripper/config.fish
@@ -2,7 +2,14 @@
 #   set -x (gnome-keyring-daemon --start | string split '=')
 # end
 
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+# performing any auto-ssh-add operations with GCR causes a stalled process with 100% CPU usage
+# service is typically enabled in the global user scope, but has been disabled temporarily via
+# sudo systemctl disable --user --global gcr-ssh-agent.socket
+#set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+
+# using OpenSSH's ssh-agent until GCR can fix bug with ssh-add
+# systemctl enable --user ssh-agent.service
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
 # "equivalent" programs
 alias dig='drill'
