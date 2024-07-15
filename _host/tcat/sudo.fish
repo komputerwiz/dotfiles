29ea61f1ff08b0b70aa0a44f7228b52e9ce4957b
Matthew Barry <mbarry@tamu.edu>
2024-07-15 15:48:28 -0500

    [@tcat] check if password file exists before reading it

--- _host/tcat/sudo.fish
+++ _host/tcat/sudo.fish
@@ -1,5 +1,6 @@
 function sudo
-  set -lx LC_PASSWORD (gpg --decrypt --quiet "$sudo_password_file" | base64 --wrap=0)
+  set -q sudo_password_file
+  and set -lx LC_PASSWORD (gpg --decrypt --quiet "$sudo_password_file" | base64 --wrap=0)
 
   set -l command
   if test (count $argv) -gt 0
