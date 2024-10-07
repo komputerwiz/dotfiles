4d373e9ad274a15ce8685cf2061e5fbb4d18f450
Matthew Barry <mbarry@tamu.edu>
2024-10-07 10:37:13 -0500

    [@tcat] sudo updates

--- _host/tcat/sudo.fish
+++ _host/tcat/sudo.fish
@@ -1,12 +1,12 @@
-function sudo
-  set -q sudo_password_file
-  and set -lx LC_PASSWORD (gpg --decrypt --quiet "$sudo_password_file" | base64 --wrap=0)
-
-  set -l command
-  if test (count $argv) -gt 0
-    set command "cd '$PWD' && exec bash -lc 'sudo --askpass $argv'"
-  end
-
-  ssh -o SendEnv=LC_PASSWORD sudo $command
-end
-
+alias sudo=doas
+# function sudo
+#   set -q sudo_password_file
+#   and set -lx LC_PASSWORD (gpg --decrypt --quiet "$sudo_password_file" | base64 --wrap=0)
+#
+#   set -l command
+#   if test (count $argv) -gt 0
+#     set command "cd '$PWD' && exec bash -lc 'sudo --askpass $argv'"
+#   end
+#
+#   ssh -o SendEnv=LC_PASSWORD sudo $command
+# end
--- _host/tcat/updater-sudo-shim.sh
+++ _host/tcat/updater-sudo-shim.sh
@@ -2,5 +2,6 @@ SUDO_PASSWORD_FILE=${SUDO_PASSWORD_FILE:-"$HOME/.local/share/creds/sudo.gpg"}
 export LC_PASSWORD="$(gpg --decrypt --quiet "$SUDO_PASSWORD_FILE" | base64 --wrap=0)"
 
 sudo() {
-  ssh -o SendEnv=LC_PASSWORD sudo "cd '$PWD' && bash -lc 'sudo --askpass $@'"
+  # ssh -o SendEnv=LC_PASSWORD sudo "cd '$PWD' && bash -lc 'sudo --askpass $@'"
+  doas "$@"
 }
