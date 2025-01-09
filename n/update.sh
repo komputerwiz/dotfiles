8dad17d3d95f78fe495a77861c1367ddfbdab91e
Matthew Barry <komputerwiz.matt@gmail.com>
2025-01-09 10:31:08 -0600

    [n] ensure latest versions of Node.JS are installed on update; allow user-/host-specific version config

--- n/update.sh
+++ n/update.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 
+# self-update
 n-update -y
+
+# user-defined versions
+VERSIONS_FILE="${XDG_CONFIG_HOME:-"$HOME/.config"}/n/versions"
+if test -f "$VERSIONS_FILE"; then
+	grep -v '^[[:space:]]*\(#\|$\)' "$VERSIONS_FILE" | while read VERSION; do
+		n "$VERSION"
+	done
+fi
+
+# ensure most recent `lts` and `latest` are installed; leave `latest` as active
+n lts
+n latest
