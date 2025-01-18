1722f4ce3059150bb115d1310112039857cf0c68
Matthew Barry <komputerwiz.matt@gmail.com>
2025-01-18 13:18:11 -0600

    [zsh] minor fix to install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,6 +3,6 @@
 [ -z "$(which zsh)" ] && echo "ZSH is not installed! Exiting." >&2 && exit 1
 
 echo "Installing Antigen..."
-curl https://git.io/antigen "$HOME/.zsh/antigen.zsh"
+curl -L https://git.io/antigen > "$HOME/.zsh/antigen.zsh"
 
 echo "Installation complete. Restart your shell to initialize the awesome!"
