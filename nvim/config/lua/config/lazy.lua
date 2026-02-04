1d138e95ec52ecadb47727a6e8df38c31da7495f
Matthew Barry <komputerwiz.matt@gmail.com>
2026-02-03 21:23:51 -0600

    [nvim] update lazy.nvim config

--- nvim/config/lua/config/lazy.lua
+++ nvim/config/lua/config/lazy.lua
@@ -18,12 +18,19 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require('lazy').setup({
 	spec = {
-		-- import your plugins
+		-- load lua/plugins/*.lua
 		{ import = 'plugins' },
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { 'habamax' } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+	install = {
+		-- install missing plugins on startup
+		missing = true,
+		-- colorscheme that will be used when installing plugins.
+		colorscheme = { 'habamax' },
+	},
+	checker = {
+		-- automatically check for plugin updates on startup
+		enabled = true,
+		-- only once per day
+		frequency = 86400,
+	},
 })
